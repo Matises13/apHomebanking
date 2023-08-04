c7ed9d987db6fc426885bb5d9c900d751c86d16e
Marcelo Matias Gonzalez <gonzalezmarcelomatias@gmail.com>
2023-08-04 18:19:15 -0300

    First Commit Task1

---
+++ src/main/java/com/ap/homebanking_ap/HomebankingApApplication.java
@@ -0,0 +1,31 @@
+package com.ap.homebanking_ap;
+
+import com.ap.homebanking_ap.models.Client;
+import com.ap.homebanking_ap.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HomebankingApApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebankingApApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initData(ClientRepository clientRepository){
+		return (args -> {
+			Client client1 = new Client();
+			client1.setFirstName("Melba");
+
+
+			clientRepository.save(client1);
+		});
+	}
+
+
+
+
+}
---
+++ src/main/java/com/ap/homebanking_ap/models/Client.java
@@ -0,0 +1,48 @@
+package com.ap.homebanking_ap.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private String firstName;
+    private String secondName;
+    private String email;
+
+    public Client(){}
+    public Client(String firstName, String secondName, String email) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.email = email;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
