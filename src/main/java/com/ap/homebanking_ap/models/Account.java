9b5b8c547cb8a68299a7df2fa0767cb3d01fda5b
Marcelo Matias Gonzalez <gonzalezmarcelomatias@gmail.com>
2023-08-08 18:07:41 -0300

    Iniciando Task2

---
+++ src/main/java/com/ap/homebanking_ap/models/Account.java
@@ -0,0 +1,20 @@
+package com.ap.homebanking_ap.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private String number;
+    
+
+}
