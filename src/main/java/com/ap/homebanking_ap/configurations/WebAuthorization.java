7a7d4109482a4364f12aac8d08902d616c910151
Marcelo Matias Gonzalez <gonzalezmarcelomatias@gmail.com>
2023-09-04 20:59:01 -0300

    Finalizando Task6

--- src/main/java/com/ap/homebanking_ap/configurations/WebAuthorization.java
+++ src/main/java/com/ap/homebanking_ap/configurations/WebAuthorization.java
@@ -1,9 +1,12 @@
 package com.ap.homebanking_ap.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
@@ -13,12 +16,15 @@ import javax.servlet.http.HttpSession;
 
 @EnableWebSecurity
 @Configuration
-public class WebAuthorization extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
+public class WebAuthorization {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeRequests()
-                .antMatchers("/admin/**").hasAuthority("ADMIN")
-                .antMatchers("/**").hasAuthority("USER");
+                .antMatchers("/web/index.html","/web/css/**","/web/img/**","/web/js/**").permitAll()
+                .antMatchers(HttpMethod.POST,"/api/login","/api/logout").permitAll()
+                .antMatchers(HttpMethod.POST,"/api/clients").permitAll()
+                .antMatchers("/admin/**","/rest/**","/h2-console").hasAuthority("ADMIN")
+                .antMatchers("/**","/api/clients/current").hasAuthority("CLIENT");
 
         http.formLogin()
                 .usernameParameter("name")
@@ -29,7 +35,7 @@ public class WebAuthorization extends WebSecurityConfigurerAdapter {
 
         http.csrf().disable();
 
-        httpSecurity.headers().frameOptions().disable();
+        http.headers().frameOptions().disable();
 
         http.exceptionHandling().authenticationEntryPoint((req, res, exc)
                 -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
@@ -38,6 +44,8 @@ public class WebAuthorization extends WebSecurityConfigurerAdapter {
         http.formLogin().failureHandler((req, res, exc)
                 -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
         http.logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());
+
+        return http.build();
     }
     private void clearAuthenticationAttributes(HttpServletRequest request) {
 
--- src/main/java/com/ap/homebanking_ap/controllers/ClientController.java
+++ src/main/java/com/ap/homebanking_ap/controllers/ClientController.java
@@ -4,9 +4,12 @@ import com.ap.homebanking_ap.dtos.ClientDTO;
 import com.ap.homebanking_ap.models.Client;
 import com.ap.homebanking_ap.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,6 +19,10 @@ import java.util.stream.Collectors;
 public class ClientController {
     @Autowired
     private ClientRepository clientRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
 
     @RequestMapping("/clients")
     private List<ClientDTO> getClients(){
@@ -24,5 +31,26 @@ public class ClientController {
     @RequestMapping("/clients/{id}")
     private ClientDTO getId(@PathVariable Long id){
         return new ClientDTO(clientRepository.findById(id).orElse(null));
+    }
+
+    @RequestMapping(value = "/clients/current", method = RequestMethod.GET)
+    public ClientDTO getCurrent (Authentication authentication){
+        return new ClientDTO(clientRepository.findByEmail(authentication.getName()));
+    }
+    @RequestMapping(path = "/clients", method = RequestMethod.POST)
+    public ResponseEntity<Object> register(
+            @RequestParam String firstName, @RequestParam String lastName, @RequestParam String email, @RequestParam String password){
+
+        if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty()){
+            return new ResponseEntity<>("Missing Data", HttpStatus.FORBIDDEN);
+        }
+
+        if (clientRepository.findByEmail(email) != null){
+            return new ResponseEntity<>("Name already in use", HttpStatus.FORBIDDEN);
+        }
+
+        clientRepository.save(new Client(firstName, lastName, email, passwordEncoder.encode(password)));
+        return new ResponseEntity<>(HttpStatus.CREATED);
+
     }
 }
